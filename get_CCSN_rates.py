c05ef1d0a58623c5fef047f65d046e1d6d01a3a4
Selhedri Selhedri <selhedri@cca001.in2p3.fr>
2022-01-20 22:49:23 +0100

    Setting up the working repository

---
+++ get_CCSN_rates.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import sys
+import os
+current_dir = os.getcwd()
+sys.path=[f"{current_dir}/snewpy/python/"]+sys.path
+from snewpy import snowglobes
+import astropy.units as u
+
+SNOwGLoBES_path = f"{current_dir}/snowglobes/"  # where the detector and cross-section files are located
+
+# arguments for generate_time_series
+model_dir = f"{current_dir}/SNEWPY_models/Bollig_2016/"
+model_prefix = "s11.2c"
+model_file = f"{model_dir}/{model_prefix}"
+modeltype = 'Bollig_2016'
+transformation = 'AdiabaticMSW_NMO'
+d = 10  # Supernova distance in kpc
+deltat = 0.1 * u.s
+output_folder = "."
+
+# Running the modules
+outfile = snowglobes.generate_time_series(model_file, modeltype, transformation, d, deltat = deltat)
+snowglobes.simulate(SNOwGLoBES_path, outfile, detector_input="all", detector_effects=False)
+snowglobes.collate(SNOwGLoBES_path, outfile, detector_input="all", smearing=False, skip_plots=True)
+
+# Save to ROOT histograms
+infile=outfile.replace('.tar.bz2', '_SNOprocessed.tar.gz')
+os.system(f"{current_dir}/snewpy/python/snewpy/scripts/Convert_to_ROOT.py '{infile}'")
+
+# Copy in safe place and cleanup
+os.system(f"mv {model_dir}/*_unsmeared_weighted.root {output_folder}")
+os.system(f"rm {model_dir}/*.root")
+os.system(f"rm {model_dir}/*.tar*")
---
+++ snowglobes/README.md
@@ -0,0 +1,7 @@
+# Detector file, channel list, and cross-sections from SNOwGLoBES
+
+Contains detection and interaction information to compute expected interaction rates using SNEWPY:
+
+* detector_configurations.dat : detector masses (total mass, and mass of reference target)
+* xscns: interaction cross-section (the files contain two columns: log_10(E/1GeV) and sigma(E)/(E [GeV]))
+* channels: information about interaction channels for each detection material (flavor, parity, weighting factor compared to reference target)
