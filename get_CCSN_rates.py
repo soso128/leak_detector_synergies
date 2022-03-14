a7487a65bc7fe48bceca183e9d03953acc1a4d91
Selhedri Selhedri <selhedri@cca001.in2p3.fr>
2022-03-14 15:53:01 +0100

    Fix a few options in rate computation code

--- get_CCSN_rates.py
+++ get_CCSN_rates.py
@@ -18,11 +18,12 @@ transformation = 'AdiabaticMSW_NMO'
 d = 10  # Supernova distance in kpc
 deltat = 0.1 * u.s
 output_folder = "."
+detector="wc100kt15prct"
 
 # Running the modules
 outfile = snowglobes.generate_time_series(model_file, modeltype, transformation, d, deltat = deltat)
-snowglobes.simulate(SNOwGLoBES_path, outfile, detector_input="all", detector_effects=False)
-snowglobes.collate(SNOwGLoBES_path, outfile, detector_input="all", smearing=False, skip_plots=True)
+snowglobes.simulate(SNOwGLoBES_path, outfile, detector_input=detector, detector_effects=False)
+snowglobes.collate(SNOwGLoBES_path, outfile, detector_input=detector, smearing=False, skip_plots=True)
 
 # Save to ROOT histograms
 infile=outfile.replace('.tar.bz2', '_SNOprocessed.tar.gz')
@@ -30,5 +31,7 @@ os.system(f"{current_dir}/snewpy/python/snewpy/scripts/Convert_to_ROOT.py '{infi
 
 # Copy in safe place and cleanup
 os.system(f"mv {model_dir}/*_unsmeared_weighted.root {output_folder}")
+os.system(f"mv {model_dir}/*.tar.bz2 {output_folder}")
 os.system(f"rm {model_dir}/*.root")
 os.system(f"rm {model_dir}/*.tar*")
+os.system(f"rm {model_dir}/*.npy")
